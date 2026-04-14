99e2b0369e230750bdff32034008aed8462200d7
daichit <gene.sixteen.000925@outlook.jp>
2026-04-14 22:01:57 +0700

    add forgot-password and /reset-password added to permitAll() access permission

--- src/main/java/com/example/cybersec/common/config/SecurityConfig.java
+++ src/main/java/com/example/cybersec/common/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/api/search").permitAll()
                         .requestMatchers("/css/**", "/images/**", "/js/**", "/compete-static/**").permitAll()
-                        .requestMatchers("/", "/login", "/register", "/learn-guest/**").permitAll()
+                        .requestMatchers("/", "/login", "/register", "/forgot-password", "/reset-password", "/learn-guest/**").permitAll()
                         .requestMatchers("/api/dashboard/**").hasAnyAuthority("USER", "LEARNER", "ADMIN")
                         .requestMatchers("/api/compete/**", "/api/labs/**", "/api/quizzes/**", "/api/progress/**")
                                 .hasAnyAuthority("USER", "LEARNER", "ADMIN")
